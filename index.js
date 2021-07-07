42a193c25bad543ed398153ffd0c36d1814e0281
djsteve33 <swomble@aol.com>
2021-07-07 14:48:59 -0400

    commit tree b3e48c497238bb0f97f0ec677264fa7a660e3848

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+function map(arr, callback) {
+    const a = [];
+    for (let i = 0; i < arr.length; i++) {
+        const myElement = arr[i];
+        a.push(callback(myElement));
+    }
+    return a;
+}
+
+function reduce(arr, callback, startingValue) {
+    let total;
+    if (startingValue) {
+        total = startingValue;
+        for (let i = 0; i < arr.length; i++) {
+            total = callback(arr[i], total);
+        }
+        return total;
+    } else {
+        total = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+            total = callback(arr[i], total);
+        }
+        return total;
+    }
+}
